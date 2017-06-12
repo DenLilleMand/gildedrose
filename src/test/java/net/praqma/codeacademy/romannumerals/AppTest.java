3aae1da4a3e8596ab1ff1b44b029c1935e04727a
Sofus Albertsen <soa@praqma.net>
2017-06-12 22:30:32 +0200

    initial commit

---
+++ src/test/java/net/praqma/codeacademy/romannumerals/AppTest.java
@@ -0,0 +1,14 @@
+package net.praqma.codeacademy.romannumerals;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void testConvert() {
+		assertEquals("I", App.convert(1));
+	}
+
+}
